d01b74f4d5910534c4837d03eea65d0d008fb856
Daniel Porzio <daniel.porzio@moove-it.com>
2019-04-18 19:43:01 -0300

    Add mapbox and update styles loader for scss

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,12 @@ module.exports = {
         use: ["babel-loader"]
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
       },
       {
         test: /\.(gif|png|jpe?g|svg|otf|ttf)$/i,
