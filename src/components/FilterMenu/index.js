2c18dd91f934f7058977dd619dbe3660d7a2ac5e
Daniel Porzio <daniel.porzio@moove-it.com>
2019-07-15 20:11:29 -0300

    Add filtering logic

--- src/components/FilterMenu/index.js
+++ src/components/FilterMenu/index.js
@@ -10,7 +10,8 @@ class FilterMenu extends React.Component {
   constructor() {
     super();
     this.state = {
-      primaryType: [
+      primaryTypes: [
+        'All',
         'THEFT',
         'BATTERY',
         'CRIMINAL DAMAGE',
@@ -24,7 +25,8 @@ class FilterMenu extends React.Component {
         'CRIMINAL TRESPASS',
         'WEAPONS VIOLATION'
       ],
-      year: [
+      years: [
+        'All',
         '2002',
         '2001',
         '2003',
@@ -38,7 +40,8 @@ class FilterMenu extends React.Component {
         '2011',
         '2012'
       ],
-      description: [
+      descriptions: [
+        'All',
         'SIMPLE',
         '$500 AND UNDER',
         'DOMESTIC BATTERY SIMPLE',
@@ -52,7 +55,8 @@ class FilterMenu extends React.Component {
         'RETAIL THEFT',
         'TELEPHONE THREAT'
       ],
-      locationDescription: [
+      locationDescriptions: [
+        'All',
         'STREET',
         'RESIDENCE',
         'APARTMENT',
@@ -66,11 +70,13 @@ class FilterMenu extends React.Component {
         'RESIDENCE PORCH/HALLWAY',
         'VEHICLE NON-COMMERCIAL',
       ],
-      arrest: [
+      arrests: [
+        'All',
         'True',
         'False'
       ],
-      domestic: [
+      domestics: [
+        'All',
         'True',
         'False'
       ]
@@ -81,13 +87,32 @@ class FilterMenu extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  prepareCrimeFilters = () => {
+    const { primaryType, year, description, arrest, location, domestic } = this.state
+    const params = {
+      primary_type: primaryType,
+      year: year,
+      description: description,
+      arrest: arrest,
+      location_description: location,
+      domestic: domestic
+    };
+    const filteredParams = Object.keys(params).reduce( (previous, key) => {
+      if (typeof params[key] !== 'undefined' && params[key] !== 'All') {
+        previous[key] = params[key];
+      }
+      return previous;
+    }, {});
+    this.props.getCrimes(filteredParams);
+  }
+
   render() {
     return (
       <div className='filter-menu'>
         <InputLabel htmlFor="year-label">Year</InputLabel>
         <Select
           className='filter-menu__select'
-          value={this.state.year}
+          value={this.state.year || 'All'}
           onChange={this.handleChange}
           inputProps={{
             name: 'year',
@@ -104,7 +129,7 @@ class FilterMenu extends React.Component {
         <InputLabel htmlFor="crimeType-label">Crime type</InputLabel>
         <Select
           className='filter-menu__select'
-          value={this.state.crimeType}
+          value={this.state.crimeType || 'All'}
           onChange={this.handleChange}
           inputProps={{
             name: 'crimeType',
@@ -112,15 +137,79 @@ class FilterMenu extends React.Component {
           }}
         >
           {
-            this.state.crimeTypes.map(type => {
+            this.state.primaryTypes.map(type => {
               return <MenuItem key={type} value={type}>{type}</MenuItem>;
+            })
+          }
+        </Select>
+        <InputLabel htmlFor="description-label">Description</InputLabel>
+        <Select
+          className='filter-menu__select'
+          value={this.state.description || 'All'}
+          onChange={this.handleChange}
+          inputProps={{
+            name: 'description',
+            id: 'description-select',
+          }}
+        >
+          {
+            this.state.descriptions.map(description => {
+              return <MenuItem key={description} value={description}>{description}</MenuItem>;
+            })
+          }
+        </Select>
+        <InputLabel htmlFor="locationDescriptions-label">Location</InputLabel>
+        <Select
+          className='filter-menu__select'
+          value={this.state.location || 'All'}
+          onChange={this.handleChange}
+          inputProps={{
+            name: 'location',
+            id: 'location-select',
+          }}
+        >
+          {
+            this.state.locationDescriptions.map(location => {
+              return <MenuItem key={location} value={location}>{location}</MenuItem>;
+            })
+          }
+        </Select>
+        <InputLabel htmlFor="arrests-label">Arrest</InputLabel>
+        <Select
+          className='filter-menu__select'
+          value={this.state.arrest || 'All'}
+          onChange={this.handleChange}
+          inputProps={{
+            name: 'arrest',
+            id: 'arrest-select',
+          }}
+        >
+          {
+            this.state.arrests.map(arrest => {
+              return <MenuItem key={arrest} value={arrest}>{arrest}</MenuItem>;
+            })
+          }
+        </Select>
+        <InputLabel htmlFor="domestics-label">Domestic</InputLabel>
+        <Select
+          className='filter-menu__select'
+          value={this.state.domestic || 'All'}
+          onChange={this.handleChange}
+          inputProps={{
+            name: 'domestic',
+            id: 'domestic-select',
+          }}
+        >
+          {
+            this.state.domestics.map(domestic => {
+              return <MenuItem key={domestic} value={domestic}>{domestic}</MenuItem>;
             })
           }
         </Select>
         <Button
           variant="contained"
           className='filter-menu__button'
-          onClick={this.props.getCrimes}>
+          onClick={this.prepareCrimeFilters}>
           Submit
         </Button>
       </div>
