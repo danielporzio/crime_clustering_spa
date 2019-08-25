fb46c76d1a170bc97d8d8526769f78f45e06cafa
Daniel Porzio <daniel.porzio@moove-it.com>
2019-08-25 17:36:15 -0300

    Fix clusters info container not updating (#13)
    * Fix clusters info container not updating
    
    * Remove unnecessary header on crimes request (#14)

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -17,10 +17,10 @@ class Dashboard extends React.Component {
   }
 
   getCrimes = params => {
+    this.setState({ clearClustersInfo: true });
     const crimesURL = `${Env.getCurrent().api.dataURL}/crimes`;
     axios
       .get(crimesURL, {
-        headers: { 'Access-Control-Allow-Origin': '*' },
         params: params,
         paramsSerializer: params => {
           return qs.stringify(params, { arrayFormat: 'repeat' });
@@ -28,6 +28,7 @@ class Dashboard extends React.Component {
       })
       .then(response => {
         this.setState({ crimes: response.data });
+        this.setState({ clustersInfo: false });
       });
   };
 
@@ -35,7 +36,7 @@ class Dashboard extends React.Component {
     return (
       <div className='dashboard-body'>
         <FilterMenu getCrimes={this.getCrimes}/>
-        <Map crimes={this.state.crimes}/>
+        <Map crimes={this.state.crimes} clearClustersInfo={this.state.clearClustersInfo}/>
       </div>
     );
   }
